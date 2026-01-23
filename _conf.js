e91288a0b70c706be0642b95258263ab3846920d
JudithdJHS <132173175+JudithdJHS@users.noreply.github.com>
2026-01-23 17:58:42 +0100

    Update _conf.js

--- _conf.js
+++ _conf.js
@@ -7,7 +7,7 @@
           messagingSenderId: "641556114718",
           appId: "1:641556114718:web:0de23bcb20481b19a86d0f"
         }; 
-export const senmailer = async (formData) =>{
+const senmailer = async (formData) =>{
  alert('entra');
  console.log(formData);
       await fetch("https://connect.mailerlite.com/api", {
