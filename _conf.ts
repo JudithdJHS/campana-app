3bb5b34e739d78e4270f33b5d4558e4cf6262207
JudithdJHS <132173175+JudithdJHS@users.noreply.github.com>
2026-01-23 16:11:49 +0100

    Update _conf.ts

--- _conf.ts
+++ _conf.ts
@@ -1,1 +1,9 @@
-
+ // TUS CLAVES REALES
+        const firebaseConfig = {
+          apiKey: "AIzaSyCO9aonuABDfwjRjmUTKnPGgKRfNVP5VjU",
+          authDomain: "campana-fundacion.firebaseapp.com",
+          projectId: "campana-fundacion",
+          storageBucket: "campana-fundacion.firebasestorage.app",
+          messagingSenderId: "641556114718",
+          appId: "1:641556114718:web:0de23bcb20481b19a86d0f"
+        };
