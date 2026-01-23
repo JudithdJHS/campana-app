69f1f3dc8d35146b0e4ffa901c09cd20df261311
JudithdJHS <132173175+JudithdJHS@users.noreply.github.com>
2026-01-23 18:06:55 +0100

    Update _conf.js

--- _conf.js
+++ _conf.js
@@ -7,5 +7,19 @@
           messagingSenderId: "641556114718",
           appId: "1:641556114718:web:0de23bcb20481b19a86d0f"
         }; 
-
+const senmailer = async (formData) =>{
+ alert('entra');
+ console.log(formData);
+      await fetch("https://connect.mailerlite.com/api", {
+        method: "POST",
+        headers: { "Content-Type": "application/json",
+                 "Authorization": ""},
+        body: JSON.stringify({
+          name: formData.name,
+          email: formData.email.toLowerCase(),
+          origen: "toca_la_campana",
+          fecha: new Date().toISOString()
+        })
+      });
+}
 
